0db8da020c74e5032241a981e6a9d839828d106c
untalinfo <racso1607@gmail.com>
2020-04-27 21:51:57 +0000

    task 10 added

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function that check if a singly linked list has a cycle
+ * @list: pointer list to check
+ * Return: 0 if there is no cycle or 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	lists_t *tmp1 = list;
+	lists_t *tmp2 = list;
+
+	if (list == NULL)
+		return (0);
+	while (tmp1 != NULL && tmp2 != NULL && tmp2->next != NULL)
+	{
+		tmp1 = tmp1->next;
+		tmp2 = tmp2->netx->next;
+		if (tmp1 == tmp2)
+			return (1);
+	}
+	return (0);
+}
