b3790b34a9333b1f07e832cdbad991c0f1ad1f32
untalinfo <racso1607@gmail.com>
2020-04-28 20:24:00 +0000

    fix style pep8 in task 6, 12, 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for i in range(len(str)):
         a = ord(str[i])
         if a > 96 and a < 123:
-           a = a - 32
+            a = a - 32
         print("{}".format(chr(a)), end="")
     print()
