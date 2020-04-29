c4ef1e24c73811a6f98c47b3367296e563f9de96
untalinfo <racso1607@gmail.com>
2020-04-29 22:31:51 +0000

    task avanced added second death line

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -19,7 +19,7 @@ listint_t *insert_node(listint_t **head, int number)
 		return (NULL);
 
 	new->n = number;
-	if(*head == NULL || (*head)->n >= number)
+	if (*head == NULL || (*head)->n >= number)
 	{
 		new->next = *head;
 		*head = node;
