c9e980cdf97a4aa3dddf60da755646f22c3671c1
untalinfo <racso1607@gmail.com>
2020-04-30 23:10:20 +0000

    fix task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         if len(argv) - 1 == 1:
             print("{} argument:".format(len(argv) - 1))
         else:
-            print("{} argument:".format(len(argv) - 1))
+            print("{} arguments:".format(len(argv) - 1))
         for i in range(len(argv) - 1):
             print("{}: {}".format(i + 1, argv[i + 1]))
     else:
