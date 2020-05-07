a25307bd2978464439a9aebc64b1fa8fb02fdd0e
untalinfo <racso1607@gmail.com>
2020-05-07 20:30:47 +0000

    fix in task0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     n_matrix = []
-    for i in matrix:
-        n_matrix.append([j**2 for j in i])
+    for row in matrix:
+        n_matrix.append([j**2 for j in row])
     return n_matrix
