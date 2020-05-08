cde788f71d39bbf7f945a625ea23a72d57611dc9
untalinfo <racso1607@gmail.com>
2020-05-08 02:16:09 +0000

    task 11 and 12 added

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not str(roman_string).isalpha() or roman_string is None:
+        return 0
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':1000}
+    total = 0
+    previous = 0
+    for i in roman_string:
+        current = romans.get(i, 0)
+        if current <= previous or previous == 0:
+            total += current
+        else:
+            total += current - (previous * 2)
+        previous = romans.get(i, 0)
+    return total
