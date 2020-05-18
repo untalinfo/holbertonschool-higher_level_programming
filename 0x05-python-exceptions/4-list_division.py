6a60d224f5921972b4356a07b72280a9f39854a2
untalinfo <racso1607@gmail.com>
2020-05-18 19:34:54 +0000

    task4 - task6 added pep8 style ok

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    
+    newList = []
+    for i in range(list_length):
+        res = 0
+        try:
+            res = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        finally:
+            newList.append(res)
+    return newList
