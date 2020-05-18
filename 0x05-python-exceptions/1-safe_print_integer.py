9435e02f8c9a87c9a8f51684aa86f3b126cf37f2
untalinfo <racso1607@gmail.com>
2020-05-18 17:55:16 +0000

    firts commit

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        int(value)
+        print("{}".format(value), end="")
+        print()
+        return True 
+    except ValueError:
+        return False
