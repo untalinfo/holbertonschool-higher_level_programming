57ef2b0eae54dc31355603cb39a9283446c8ba72
untalinfo <racso1607@gmail.com>
2020-05-19 20:53:24 +0000

    docstring added in taks

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 class Square:
+    """A square class."""
     def __init__(self):
+        """Initialization square."""
         pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 class Square:
+    """A square class."""
     def __init__(self, size):
+        """Initialization square."""
         self.__size = size
