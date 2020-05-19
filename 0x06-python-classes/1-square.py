a025f25a16cb3481a8cfcf35b1118e3ad9164075
untalinfo <racso1607@gmail.com>
2020-05-19 21:17:01 +0000

    docstring added in task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""This module create a new class Square"""
+
+
 class Square():
     """A square class."""
-
-
     def __init__(self, size):
         """Initialization square."""
         self.__size = size
