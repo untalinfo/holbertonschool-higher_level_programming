a9da15e9464b576baef434c35b6d6613ff0f935c
untalinfo <racso1607@gmail.com>
2020-05-20 03:12:48 +0000

    fix validation value[1]

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -62,9 +62,9 @@ class Square():
     @position.setter
     def position(self, value):
         """Setter function of position. position of the square"""
-        if type(value) != tuple or type(value[0], value[1]) != int:
+        if type(value) != tuple or type(value[0]) != int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if type(value) != tuple or len(value) != 2:
+        if type(value[1]) != int or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
