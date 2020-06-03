4a58c954d5a4e32d103f1faad73a9e0b318719e2
untalinfo <racso1607@gmail.com>
2020-06-03 23:36:11 +0000

    fix in task8

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -6,5 +6,5 @@ import json
 
 
 def load_from_json_file(filename):
-    with open(filename, encoding='UTF-8') as f:
-        return json.load(f)
+    with open(filename, 'r', encoding='UTF-8') as f:
+        return json.loads(f.read())
