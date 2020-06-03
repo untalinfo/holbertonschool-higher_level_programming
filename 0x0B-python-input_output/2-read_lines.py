cd3fb12ef4ddc1355af205e1e95dea5c4f2c8c6b
untalinfo <racso1607@gmail.com>
2020-06-03 23:58:31 +0000

    fix2

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -13,9 +13,9 @@ def read_lines(filename="", nb_lines=0):
     """
     with open(filename) as f:
         if nb_lines <= 0 or nb_lines >= len(f.readlines()):
-            nb_lines = len(f.readlines())
             for i in range(nb_lines):
                 print(f.readline(), end='')
+            nb_lines = len(f.readlines())
         f.seek(0)
         for i in range(nb_lines):
             print(f.readline(), end='')
