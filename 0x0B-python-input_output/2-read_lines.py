dc77c8fe88f48b10400750a6eb51d4fbe6565919
untalinfo <racso1607@gmail.com>
2020-06-03 23:42:17 +0000

    fix in task 2

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -11,7 +11,7 @@ def read_lines(filename="", nb_lines=0):
         filename {str} -- text file (default: {""})
         nb_lines {int} -- number of lines (default: {0})
     """
-    with open(filename, 'r', encoding='UTF-8') as f:
+    with open(filename, 'r') as f:
         if nb_lines <= 0 or nb_lines <= (len(f.readlines())):
             nb_lines = len(f.readlines())
         f.seek(0)
