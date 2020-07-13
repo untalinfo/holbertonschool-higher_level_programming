2e74a2671f580b7faa0c03a76d6e4c1528d6c598
untalinfo <racso1607@gmail.com>
2020-07-13 20:24:29 +0000

    task3 and task4 add list all tables and create table

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- script that lists all the tables of a database in your MySQL server.
+--  display all tables of specific database
+SHOW TABLES;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- script that creates a table called first_table in the current database in your MySQL server.
+-- create table into current database
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
