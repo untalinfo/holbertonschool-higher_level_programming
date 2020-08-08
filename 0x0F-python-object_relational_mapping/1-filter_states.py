b4f63e3fd2922068a750c6a7c2f3e4c5f2e7f9ae
untalinfo <racso1607@gmail.com>
2020-08-08 15:10:37 +0000

    fix in task1, correction Like binary

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
                          user=username, passwd=password, db=dataBase)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
+        ORDER BY id ASC;")
     rows = cur.fetchall()
     for row in rows:
         print(row)
