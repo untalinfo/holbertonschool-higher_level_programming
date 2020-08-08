fd6e4fc98c2dc5c15eae50e90a858ec075619ed7
untalinfo <racso1607@gmail.com>
2020-08-08 15:24:56 +0000

    task6 - task9 added

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+lists all State objects from the database hbtn_0e_6_usa
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dataBase = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, dataBase),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+lists all State objects from the database hbtn_0e_6_usa
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dataBase = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, dataBase),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    if session.query(State).first():
+        print("1: " + session.query(State).first().name)
+    else:
+        print("Nothing")
+    session.close()
