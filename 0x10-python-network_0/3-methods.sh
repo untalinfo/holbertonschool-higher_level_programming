8fa52e8b619fa80e20a4f13ee246d052717727b8
untalinfo <racso1607@gmail.com>
2020-08-18 23:08:33 +0000

    firts commit

---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Bash script that takes in a URL and displays all HTTP methods 
+# the server will accept.
+curl -sD - "$1" | grep 'Allow: ' | cut -d' ' -f2-
---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# takes in a URL, sends a POST request to the passed URL,
+# and displays the body of the response
+curl -s -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST "$1"
