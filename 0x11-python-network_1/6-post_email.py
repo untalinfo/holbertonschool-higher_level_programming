e217f7c6d3c0791244986e85e10f3a6ba756cabb
untalinfo <racso1607@gmail.com>
2020-08-25 00:08:02 +0000

    fix line 3 pep8 style

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-takes in a URL and an email address, sends a POST request to the passed URL with
-the email as a parameter, and finally displays the body of the response
+takes in a URL and an email address, sends a POST request to the passed
+URL with the email as a parameter, and finally displays the body
+of the response
 """
 import requests
 from os import sys
