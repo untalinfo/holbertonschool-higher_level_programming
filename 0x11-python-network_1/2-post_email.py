56e4579d01ba798de8593b47ff810c56b527382f
untalinfo <racso1607@gmail.com>
2020-08-25 00:13:40 +0000

    fix tas2 pep8 style

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-takes in a URL and an email, sends a POST request to the passed URL with the email
-as a parameter, and displays the body of the response
+takes in a URL and an email, sends a POST request to the passed URL
+with the email as a parameter, and displays the body of the response
 """
 import urllib.request
 import urllib.parse
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     values = {'email': sys.argv[2]}
 
     data = urllib.parse.urlencode(values)
-    data = data.encode('ascii') # data should be bytes
+    data = data.encode('ascii')  # data should be bytes
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
