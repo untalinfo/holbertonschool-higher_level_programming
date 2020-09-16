ef4a6067c0fd00ba743fecb67ca7bcde13c52375
untalinfo <racso1607@gmail.com>
2020-09-16 01:30:31 +0000

    fix task4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (!(process.argv[3])) {
   console.log(process.argv[2] + ' is undefined');
-} else if (!(process.argv[4])) {
+} else if (!(process.argv[3])) {
   console.log('undefined is undefined');
 } else {
   console.log(process.argv[2] + ' is ' + process.argv[3]);
