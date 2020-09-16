0e393a397e9e1adfff51360b8590e81295f4a01c
untalinfo <racso1607@gmail.com>
2020-09-16 01:27:56 +0000

    fix messaje

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const parameter = parseInt(process.argv[2]);
 if (!(parameter)) {
   console.log('Not a number');
 } else if (Number.isInteger(parameter)) {
-  console.log('Number: ' + parameter);
+  console.log('My number: ' + parameter);
 } else {
   console.log('Not a number');
 }
